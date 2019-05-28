b86401d582e772465d1b0753ac3c49aa9fa406ef
einavcohen <einavcohen006@gmail.com>
2019-05-28 22:47:20 +0300

    updating Server

--- Ex3/Models/Server.cs
+++ Ex3/Models/Server.cs
@@ -13,98 +13,45 @@ namespace Ex3.Models
 {
     public class Server
     { 
-        private float lon=0.0f;
-        private float lat=0.0f;
-        private bool shouldContinue = true;
-        private string[] valuesFromSim = new string[23];
- 
-        string[] ValuesFromSim
+        private TcpClient tcpClient;
+
+        public Server()
         {
-            get {
-                return this.valuesFromSim;
-            }
-            set {
-                this.ValuesFromSim = value;
-            }
-        }
-        public float Lon
-        {
-            get
-            {
-                return lon;
-            }
-            set
-            {
-                lon = value;
-            }
+            tcpClient = new TcpClient();
         }
 
-        public float Lat
-        {
-            get
-            {
-                return lat;
-            }
-            set
-            {
-                lat = value;
-            }
-        }
         public void Connect(string ip, int port)
         {
-            shouldContinue = true;
-            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ip), port);
-            TcpListener listener = new TcpListener(ep);
-            listener.Start();
-            //MessageBox.Show("after start");
-            HandleClient(listener);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            tcpClient.Connect(endPoint);
         }
 
-        public void HandleClient(TcpListener listener)
+        public double Get(string requestPath)
         {
-            TcpClient client = listener.AcceptTcpClient();
-            //MessageBox.Show("after connect");
-            Thread thread = new Thread(() => ReadFromClient(client, listener));
-            thread.Start();
-        }
+            int size = 2048;
+            byte[] response = new byte[size];
 
-        void ReadFromClient(TcpClient client, TcpListener listener)
-        {
-            Byte[] bytes;
-            while (shouldContinue)
+            if (tcpClient.Connected)
             {
-                NetworkStream ns = client.GetStream();
-                if (client.ReceiveBufferSize > 0)
-                {
-                    bytes = new byte[client.ReceiveBufferSize];
-                    ns.Read(bytes, 0, client.ReceiveBufferSize);
-                    string msg = Encoding.ASCII.GetString(bytes); //the message incoming
-                    EditMessage(msg);
-                    //MessageBox.Show("the func");
-                }
-            }
-            client.Close();
-            listener.Stop();
-        }
+                NetworkStream stream = tcpClient.GetStream();
 
-        void EditMessage(string Message)
-        {
-            string[] splitwords = Message.Split(',');
-            this.valuesFromSim = splitwords;
-            try
-            {
-                Lon = float.Parse(splitwords[0]);
-                Lat = float.Parse(splitwords[1]);
+                byte[] encoded = Encoding.ASCII.GetBytes("get " + requestPath + "\r\n");
+                stream.Write(encoded, 0, encoded.Length);
+                stream.Flush();
+
+                stream.Read(response, 0, size);
             }
-            catch (Exception E)
-            {
-            }
-            //MessageBox.Show(valuesFromSim[0]);
+
+            // getting the value from the response format of the server .
+            string num = Encoding.ASCII.GetString(response, 0, size).Split(' ')[2];
+
+            // Get rid of quotation marks and converting the number to Double .
+            return Convert.ToDouble(num.Substring(1, num.Length - 2));
         }
 
         public void Stop()
         {
-            this.shouldContinue = false;
+            tcpClient.Close();
         }
 
     }
